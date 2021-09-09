43edbcd8df34bdb1ea73eff4d01b4abd1afea354
Ezihe Godswill <ezihegodswill01@gmail.com>
2021-09-10 00:21:52 +0100

    netlify deploy error fix-02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './Navbar';
 import Home from './pages/Home';
 
 function App() {
